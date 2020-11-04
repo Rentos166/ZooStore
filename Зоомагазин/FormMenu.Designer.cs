bbe4d994cadcd4ceb1d1d4b7ff539392ff720ea2
Rentos166 <nikola2310@mail.ru>
2020-11-05 00:13:31 +0300

    Добавьте файлы проекта.

---
+++ Зоомагазин/FormMenu.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Зоомагазин
+{
+    partial class FormMenu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.OpenCustom = new System.Windows.Forms.Button();
+            this.OpenStorage = new System.Windows.Forms.Button();
+            this.OpenProvider = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 67);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // OpenCustom
+            // 
+            this.OpenCustom.Location = new System.Drawing.Point(3, 177);
+            this.OpenCustom.Name = "OpenCustom";
+            this.OpenCustom.Size = new System.Drawing.Size(300, 42);
+            this.OpenCustom.TabIndex = 6;
+            this.OpenCustom.Text = "Заказы";
+            this.OpenCustom.UseVisualStyleBackColor = true;
+            // 
+            // OpenStorage
+            // 
+            this.OpenStorage.Location = new System.Drawing.Point(3, 129);
+            this.OpenStorage.Name = "OpenStorage";
+            this.OpenStorage.Size = new System.Drawing.Size(300, 42);
+            this.OpenStorage.TabIndex = 5;
+            this.OpenStorage.Text = "Склад";
+            this.OpenStorage.UseVisualStyleBackColor = true;
+            // 
+            // OpenProvider
+            // 
+            this.OpenProvider.Location = new System.Drawing.Point(3, 81);
+            this.OpenProvider.Name = "OpenProvider";
+            this.OpenProvider.Size = new System.Drawing.Size(300, 42);
+            this.OpenProvider.TabIndex = 4;
+            this.OpenProvider.Text = "Поставщики";
+            this.OpenProvider.UseVisualStyleBackColor = true;
+            this.OpenProvider.Click += new System.EventHandler(this.OpenProvider_Click);
+            // 
+            // FormMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 373);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.OpenCustom);
+            this.Controls.Add(this.OpenStorage);
+            this.Controls.Add(this.OpenProvider);
+            this.Name = "FormMenu";
+            this.Text = "Главная";
+            this.Load += new System.EventHandler(this.FormMenu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button OpenCustom;
+        private System.Windows.Forms.Button OpenStorage;
+        private System.Windows.Forms.Button OpenProvider;
+    }
+}
+
---
+++ Зоомагазин/FormProvider.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Зоомагазин
+{
+    public partial class FormProvider : Form
+    {
+        void ShowProviders()
+        {
+            //Предварительно очищаем listView
+            listViewProvider.Items.Clear();
+            //Проходимся по коллекции клиентов, которые находятся в базе с помощью foreach
+            foreach (ProvidersSet providerSet in Program.zooDb.ProvidersSet)
+            {
+                //создаем новый элемент в listView
+                //для этого создаем новый массив строк
+                ListViewItem item = new ListViewItem(new string[]
+                {
+                    //указываем необходимые поля
+                    providerSet.Id.ToString(),
+                    providerSet.CompanyName,
+                    "г. " + providerSet.Address_City+", " + "ул. " + providerSet.Address_Street+", "+
+                    "д. " + providerSet.Address_Home,
+                    providerSet.Phone,
+                    providerSet.Email
+                });
+                //указываем по какому тегу будем брать элементы
+                item.Tag = providerSet;
+                //добавляем элементы в listView для отображения
+                listViewProvider.Items.Add(item);
+            }
+        }
+        
+        public FormProvider()
+        {
+            InitializeComponent();
+            ShowProviders();
+        }
+
+        private void FormProvider_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Создаем новый экземпляр класса Поставщик
+                ProvidersSet providerSet = new ProvidersSet();
+                //Делаем ссылку на объект, который хранится в textBox-ax
+                providerSet.CompanyName = textBoxCompanyName.Text;
+                providerSet.Address_City = textBoxAddress_City.Text;
+                providerSet.Address_Street = textBoxAddress_Street.Text;
+                providerSet.Address_Home = textBoxAddress_Home.Text;
+                providerSet.Phone = textBoxPhone.Text;
+                providerSet.Email = textBoxEmail.Text;
+                if (providerSet.CompanyName == "" || providerSet.Address_City == "" || providerSet.Address_Street == "" || providerSet.Phone == "")
+                {
+                    throw new Exception("Данные о поставщике не заполнены");
+                }
+                //Добавляем в таблицу ProviderSet нового клиента providerSet
+                Program.zooDb.ProvidersSet.Add(providerSet);
+                //Сохраняем изменения в модели zokiDb (экземпляр которой был создан ранее)
+                Program.zooDb.SaveChanges();
+                ShowProviders();
+            }
+            catch (Exception ex) { MessageBox.Show("" + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listViewProvider.SelectedItems.Count == 1)
+                {
+                    ProvidersSet providerSet = listViewProvider.SelectedItems[0].Tag as ProvidersSet;
+                    //Делаем ссылку на объект, который хранится в textBox-ax
+                    providerSet.CompanyName = textBoxCompanyName.Text;
+                    providerSet.Address_City = textBoxAddress_City.Text;
+                    providerSet.Address_Street = textBoxAddress_Street.Text;
+                    providerSet.Address_Home = textBoxAddress_Home.Text;
+                    providerSet.Phone = textBoxPhone.Text;
+                    providerSet.Email = textBoxEmail.Text;
+                    if (providerSet.CompanyName == "" || providerSet.Address_City == "" || providerSet.Address_Street == "" || providerSet.Phone == "")
+                    {
+                        throw new Exception("Данные о поставщике не заполнены");
+                    }
+                    //Сохраняем изменения в модели zokiDb (экземпляр которой был создан ранее)
+                    Program.zooDb.SaveChanges();
+                    ShowProviders();
+                }
+            }
+            catch (Exception ex) { MessageBox.Show("" + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+
+        private void buttonDel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //если выбран 1 элемент из listView
+                if (listViewProvider.SelectedItems.Count == 1)
+                {
+                    //ищем этот элемент
+                    ProvidersSet providerSet = listViewProvider.SelectedItems[0].Tag as ProvidersSet;
+                    //удаляем из модели и базы данных
+                    Program.zooDb.ProvidersSet.Remove(providerSet);
+                    //сохраняем изменения
+                    Program.zooDb.SaveChanges();
+                    //отображаем обновленный список
+                    ShowProviders();
+                }
+                //очищаем textBox-ы
+                textBoxCompanyName.Text = "";
+                textBoxAddress_City.Text = "";
+                textBoxAddress_Home.Text = "";
+                textBoxAddress_Street.Text = "";
+                textBoxPhone.Text = "";
+                textBoxEmail.Text = "";
+
+            }
+            //если возникает какая-то ошибка, к примеру, запись используется, выводим всплывающее сообщение
+            catch
+            {
+                //вызываем метод для всплывающего окна, в котором указываем текст, заголовок, кнопку и иконку
+                MessageBox.Show("Невозможно удалить, эта запись используется!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void listViewProvider_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listViewProvider.SelectedItems.Count == 1)
+            {
+                //ищем элемент из таблицы по тегу
+                ProvidersSet providerSet = listViewProvider.SelectedItems[0].Tag as ProvidersSet;
+                //указываем, что может быть изменено
+                textBoxCompanyName.Text = providerSet.CompanyName;
+                textBoxAddress_City.Text = providerSet.Address_City;
+                textBoxAddress_Street.Text = providerSet.Address_Street;
+                textBoxAddress_Home.Text = providerSet.Address_Home;
+                textBoxPhone.Text = providerSet.Phone;
+                textBoxEmail.Text = providerSet.Email;
+            }
+            else
+            {
+                //условие, иначе, если не выбран ни один элемент, то задаем пустые поля
+                textBoxCompanyName.Text = "";
+                textBoxPhone.Text = "";
+                textBoxEmail.Text = "";
+                textBoxAddress_City.Text = "";
+                textBoxAddress_Home.Text = "";
+                textBoxAddress_Street.Text = "";
+
+            }
+        }
+
+        private void textBoxPhone_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void textBoxPhone_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && number != 8) //цифры, клавиша BackSpace 
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
